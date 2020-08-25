278420f4041f7269e9f8ac0a459cc135ffec5e74
Alex Bruns <ahb@Alexs-MacBook-Pro.local>
2020-08-25 01:30:15 -0500

    AHB

--- pages/books.tsx
+++ pages/books.tsx
@@ -241,23 +241,29 @@ function Books({ data }: { data: any[] }) {
         </h1>
         {
           <div className="grid grid-flow-row grid-cols-1 gap-8 p-4 mx-auto max-w-7xl sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-            {data.map(({ cover: { url, id } }) => (
-              <div className="relative flex items-center justify-center p-8 overflow-hidden rounded-lg shadow-lg">
-                <div className="shadow-lg">
-                  <img
-                    src={`https://www.master-7rqtwti-hmyhm4xzoek6k.us-2.platformsh.site${url}`}
-                    className="absolute inset-0 opacity-25"
-                    style={{ filter: "blur(5px) contrast(200%)" }}
-                  />
-                  <Link href={`/books/${id}`}>
+            {data.map(({ id, cover: { url } }) => {
+              console.log(id);
+              return (
+                <div
+                  key={id}
+                  className="relative flex items-center justify-center p-8 overflow-hidden rounded-lg shadow-lg"
+                >
+                  <div className="shadow-lg">
                     <img
                       src={`https://www.master-7rqtwti-hmyhm4xzoek6k.us-2.platformsh.site${url}`}
-                      className="relative z-10 object-contain transition-all duration-300 ease-in-out transform rounded-md shadow-lg cursor-pointer hover:scale-110"
+                      className="absolute inset-0 opacity-25"
+                      style={{ filter: "blur(5px) contrast(200%)" }}
                     />
-                  </Link>
+                    <Link href={`/books/${id}`}>
+                      <img
+                        src={`https://www.master-7rqtwti-hmyhm4xzoek6k.us-2.platformsh.site${url}`}
+                        className="relative z-10 object-contain transition-all duration-300 ease-in-out transform rounded-md shadow-lg cursor-pointer hover:scale-110"
+                      />
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            ))}
+              );
+            })}
           </div>
         }
       </main>
