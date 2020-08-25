6f531ec7b3faf38ed20c6c28358292ecf45d3b13
Alex Bruns <ahb@Alexs-MacBook-Pro.local>
2020-08-25 16:50:37 -0500

    AHB

--- pages/books.tsx
+++ pages/books.tsx
@@ -8,10 +8,12 @@ function ImgCard({
   id,
   url,
   height,
+  width,
 }: {
   id: string;
   url: string;
   height: number;
+  width: number;
 }) {
   return (
     <div
@@ -25,11 +27,13 @@ function ImgCard({
           style={{
             filter: "blur(5px) contrast(200%)",
           }}
-          height={height}
+          height={height.toString()}
+          width={width.toString()}
         />
         <Link as={`/books/${id}`} href="/books/[slug]">
           <img
-            height={height}
+            height={height.toString()}
+            width={width.toString()}
             src={`https://www.master-7rqtwti-hmyhm4xzoek6k.us-2.platformsh.site${url}`}
             className="relative z-10 object-contain transition-all duration-300 ease-in-out transform rounded-md shadow-lg cursor-pointer hover:scale-110"
           />
@@ -51,9 +55,15 @@ function Books({ data }: { data: any[] }) {
         </h1>
         {
           <div className="grid grid-flow-row grid-cols-1 gap-8 p-4 mx-auto max-w-7xl sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-            {data.map(({ id, cover: { url, height } }) => {
+            {data.map(({ id, cover: { url, height, width } }) => {
               return (
-                <ImgCard key={id} id={id} url={url} height={height} />
+                <ImgCard
+                  key={id}
+                  id={id}
+                  url={url}
+                  height={height}
+                  width={width}
+                />
                 // <div
                 //   key={id}
                 //   className="relative flex items-center justify-center p-8 overflow-hidden rounded-lg shadow-lg"
